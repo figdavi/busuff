81c4ea72081fe0d402cdcff502d58e143ed05c34
figdavi <figueiredodavi.dev@proton.me>
2025-12-03 02:00:50 -0300

    Remove hardcoded API URL

--- frontend/bus-tracker/src/services/api.ts
+++ frontend/bus-tracker/src/services/api.ts
@@ -1,16 +1,16 @@
-import type { RouteConfig } from '../types';
+import type { RouteConfig } from "../types";
 
 // URL do seu Backend Python (localhost ou AWS)
-const API_URL = 'http://ec2-18-221-16-56.us-east-2.compute.amazonaws.com/api/'; 
+const API_URL = "/api";
 
 export const api = {
   // Busca as rotas do banco de dados
   getRoutes: async (): Promise<RouteConfig[]> => {
     try {
       const response = await fetch(`${API_URL}/routes`);
-      if (!response.ok) throw new Error('Erro ao buscar rotas');
+      if (!response.ok) throw new Error("Erro ao buscar rotas");
       const data = await response.json();
-      
+
       // Adaptador: O banco retorna "TimeRange", o front usa "timeRange"
       return data.map((r: any) => ({
         id: r.id,
@@ -18,7 +18,7 @@ export const api = {
         destination: r.destination,
         timeRange: r.TimeRange,
         days: r.days,
-        deviceId: r.device_id // Importante para o MQTT saber qual tópico ouvir
+        deviceId: r.device_id, // Importante para o MQTT saber qual tópico ouvir
       }));
     } catch (error) {
       console.error(error);
@@ -29,13 +29,13 @@ export const api = {
   // Envia a presença para o banco
   markPresence: async (userName: string, routeId: string, deviceId: string) => {
     await fetch(`${API_URL}/presence`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ 
-        user_name: userName, 
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        user_name: userName,
         route_id: routeId,
-        device_id: deviceId 
-      })
+        device_id: deviceId,
+      }),
     });
-  }
+  },
 };
