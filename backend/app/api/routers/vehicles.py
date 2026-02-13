5bc24eb15bce080051c3770e29a6fac8e8e2ab9b
figdavi <figueiredodavi.dev@proton.me>
2026-02-12 22:04:08 -0300

    Add last-position endpoints to vehicles and routes routers
    Both routes and vehicles have last-position endpoints, since Route<->Vehicle are many-to-one. This is most likely temporary and will change to many-to-many relationship in the future

--- backend/app/api/routers/vehicles.py
+++ backend/app/api/routers/vehicles.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
 from app.core.database import SessionDep
-from app.models import Vehicle, VehiclesOut
+from app.models import Vehicle, VehicleOut, VehiclesOut, Position, PositionOut
 
 
 router = APIRouter(
@@ -14,10 +14,39 @@ router = APIRouter(
 
 @router.get("/", status_code=200, response_model=VehiclesOut)
 async def read_vehicles(session: SessionDep):
-    """
-    Retorna todas as rotas cadastradas no banco para preencher a Dashboard
-    """
     stmt = select(Vehicle).order_by(Vehicle.id)
     vehicles = session.scalars(stmt).all()
 
     return {"data": vehicles}
+
+
+@router.get("/{id}", status_code=200, response_model=VehicleOut)
+async def read_vehicle(session: SessionDep, id: str):
+    vehicle = session.get(Vehicle, id)
+
+    if not vehicle:
+        raise HTTPException(status_code=404, detail="Vehicle not found")
+
+    return vehicle
+
+
+@router.get("/{id}/last-position", status_code=200, response_model=PositionOut)
+async def read_last_position(session: SessionDep, id: str):
+    vehicle = session.get(Vehicle, id)
+
+    if not vehicle:
+        raise HTTPException(status_code=404, detail="Vehicle not found")
+
+    # limit(1): Database will sort only one row instead of whole table (even though ORM is using .scalar())
+    stmt = (
+        select(Position)
+        .where(Position.vehicle_id == id)
+        .order_by(Position.timestamp_utc.desc())
+        .limit(1)
+    )
+    last_position = session.scalar(stmt)
+
+    if not last_position:
+        raise HTTPException(status_code=404, detail="No positions found")
+
+    return last_position
